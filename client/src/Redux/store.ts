d12906cfa63be6b29d40908daecf417c7d40c586
unknown <naimurk781@gmail.com>
2024-05-19 13:03:23 +0600

    redux persist

--- client/src/Redux/store.ts
+++ client/src/Redux/store.ts
@@ -1,13 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./Feature/auth/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import { configureStore } from '@reduxjs/toolkit'
-import authSlice from './Feature/auth/authSlice'
-
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, authSlice);
 export const store = configureStore({
   reducer: {
-    auth: authSlice
+    auth: persistedReducer,
   },
-})
+});
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store";
+import { persistor, store } from "./Redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate persistor={persistor} loading={null}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
