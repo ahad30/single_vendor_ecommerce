3f950f8a6813d275c9f8ffa1e743d36ea0e4677c
Mohiminul Islam Ahad <122167069+ahad30@users.noreply.github.com>
2024-05-19 18:19:47 +0600

    ahad

--- client/src/Pages/Auth/Login.tsx
+++ client/src/Pages/Auth/Login.tsx
@@ -49,7 +49,8 @@ const Login = () => {
                                 <div className="relative">
                                     <input 
                                     {...register("password")}
-                                    autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                                    autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 
+                                    focus:outline-none focus:border-b-blue-600" placeholder="Password" />
                                     <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                                 </div>
                                 <div className="relative">
