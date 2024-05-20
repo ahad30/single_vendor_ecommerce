d0b3b4f534824a1c5f9dbfa12f15e233799219f4
engr-akramulhoque <akramulhoque.engineer@gmail.com>
2024-05-20 18:07:17 +0600

    pagination with resource complete

---
+++ Apis/database/seeders/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['id' => '1', 'name' => 'Category 1', 'image' => ''],
+            ['id' => '2', 'name' => 'Category 2', 'image' => ''],
+            ['id' => '3', 'name' => 'Category 3', 'image' => ''],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+    }
+}
