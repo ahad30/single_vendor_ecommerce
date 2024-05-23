a4e776e56d69300f85337b5e34c178ba49e5612a
monir0153 <mohammadmonir0153@gmail.com>
2024-05-23 17:57:27 +0600

    profile update

--- Apis/app/Http/Controllers/Profile/UserProfileController.php
+++ Apis/app/Http/Controllers/Profile/UserProfileController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Requests\UpdateProfileRequest;
+use App\Trait\UploadImageTrait;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class UserProfileController extends Controller
 {
+    use UploadImageTrait;
     public function profile(Request $request)
     {
         $user = $request->user();
@@ -30,5 +35,25 @@ class UserProfileController extends Controller
             }),
         ];
         return Response::success($data);
+    }
+    public function updatePassword(UpdatePasswordRequest $request)
+    {
+        $old = $request->old_password;
+        $newPassword = $request->new_password;
+
+        if (Hash::check($old, auth()->user()->password)) {
+            request()->user()->update(['password' => Hash::make($newPassword)]);
+            return Response::updated(null , "password updated successfully");
+        } else {
+            return Response::error('Your previous password doesn\'t match');
+        }
+    }
+    public function updateProfile(UpdateProfileRequest $request){
+        $user = auth()->user();
+        $validated = $request->validated();
+            $path = $this->uploadImage($request, 'image', 'assets/images/profiles', $user->image);
+            $data = array_merge($validated, ['image' => $path ?: $user->image]);
+         $user->update($data);
+        return Response::updated($user, 'Profile updated successfully');
     }
 }
