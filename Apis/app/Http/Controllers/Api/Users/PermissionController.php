8241b942cbdfb6598c2f4c95b9b796077bf77f37
engr-akramulhoque <akramulhoque.engineer@gmail.com>
2024-05-25 18:13:55 +0600

    code optimize , structure changed and user crud setup

--- Apis/app/Http/Controllers/Api/Users/PermissionController.php
+++ Apis/app/Http/Controllers/Api/Users/PermissionController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api\Users;
 
 use App\Http\Controllers\Controller;
 use Spatie\Permission\Models\Permission;
@@ -11,8 +11,8 @@ class PermissionController extends Controller
     // get all permissions
     public function index()
     {
-        if(auth()->user()->roles->isNotEmpty()){
-            $permission = Permission::get(['id','name']);
+        if (auth()->user()->roles->isNotEmpty()) {
+            $permission = Permission::get(['id', 'name']);
             return Response::success($permission);
         }
         return Response::forbidden();
