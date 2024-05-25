ab4503c1da282cb681fcfd19c83f0581f2da7ec7
unknown <naimurk781@gmail.com>
2024-05-25 18:18:08 +0600

    pagination

--- client/src/Routes/Admin.Routes.tsx
+++ client/src/Routes/Admin.Routes.tsx
@@ -1,4 +1,7 @@
-import { MdOutlineCategory } from "react-icons/md";
+import {
+  MdOutlineCategory,
+  MdOutlineShoppingCartCheckout,
+} from "react-icons/md";
 import { CiSettings } from "react-icons/ci";
 import Category from "../Pages/Dashboard/Admin/Category/Category";
 import DbBackup from "../Pages/Dashboard/Admin/Settings/DbBackup";
@@ -13,16 +16,29 @@ export const adminRoutes = [
     element: <DashboardStatics></DashboardStatics>,
     icon: <MdOutlineDashboardCustomize></MdOutlineDashboardCustomize>,
   },
+  // {
+  //   path: "categories",
+  //   label: "Category",
+  //   element: <Category></Category>,
+  //   icon: <MdOutlineCategory size={20}></MdOutlineCategory>,
+  // },
+
   {
-    path: "categories",
-    label: "Category",
-    element: <Category></Category>,
-    icon: <MdOutlineCategory size={20}></MdOutlineCategory>,
+    label: "E-commerce",
+    icon: (
+      <MdOutlineShoppingCartCheckout size={20}></MdOutlineShoppingCartCheckout>
+    ),
+    children: [
+      {
+        path: "categories",
+        label: "Category",
+        element: <Category></Category>,
+      },
+    ],
   },
 
-
   {
-    label: "Settings",
+    label: "Configuration",
     icon: <CiSettings size={20}></CiSettings>,
     children: [
       { path: "db-backup", label: "DB Backup", element: <DbBackup></DbBackup> },
