53ddb0de14b6a8bae603b2170124e003b541b8cc
engr-akramulhoque <akramulhoque.engineer@gmail.com>
2024-05-26 14:12:31 +0600

    User Crud complete

--- Apis/app/Http/Controllers/Api/Users/UserController.php
+++ Apis/app/Http/Controllers/Api/Users/UserController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers\Api\Users;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Trait\UploadImageTrait;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    use UploadImageTrait;
     /**
      * Display a listing of the resource.
      */
@@ -39,9 +44,25 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateUserRequest $request)
     {
-        //
+        try {
+            $user = [];
+            DB::transaction(function () use ($request, &$user) {
+                // create user
+                $path = $this->uploadImage($request, 'image', 'assets/images/users');
+                $user = User::create(array_merge($request->validated(), ['password' => Hash::make($request->password)], ['image' => $path]));
+
+                // assign role
+                $user->assignRole($request->role);
+
+                return $user;
+            });
+
+            return Response::created(new UserResource($user), "User successfully created");
+        } catch (\Exception $e) {
+            return Response::error("Something went wrong");
+        }
     }
 
     /**
@@ -55,9 +76,28 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(UpdateUserRequest $request, User $user)
     {
-        //
+        if ($user->is_administration == true) {
+            return Response::error("Sorry, This user cannot be editable");
+        }
+
+        try {
+            DB::transaction(function () use ($request, $user) {
+                // update user
+                $path = $this->uploadImage($request, 'image', 'assets/images/users', $user->image);
+                $data = array_merge($request->validated(), ['image' => $path ?: $user->image]);
+
+                $user->update($data);
+
+                // remove previous role and assign new role
+                $user->syncRoles($request->role);
+            });
+
+            return Response::updated(new UserResource($user), "User successfully updated");
+        } catch (\Exception $e) {
+            return Response::error("Something went wrong");
+        }
     }
 
     /**
@@ -65,6 +105,15 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        if ($user->is_administration == true) {
+            return Response::error("Sorry, This user cannot be deleteable.");
+        }
+
+        try {
+            $user->delete();
+            return Response::success(null, "User successfully deleted");
+        } catch (\Exception $e) {
+            return Response::error("Something went wrong");
+        }
     }
 }
