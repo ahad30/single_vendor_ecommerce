0bd42f1e11016eade690cd0f95d29de05a9611e1
unknown <naimurk781@gmail.com>
2024-05-26 17:17:42 +0600

    s

--- client/src/Component/Dashborad/DashboardTitle.tsx
+++ client/src/Component/Dashborad/DashboardTitle.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from "react";
 
-const DashboardTitle = ({text , children}: {text: string , children: ReactNode}) => {
-    return (
-     <h2 className="text-xl font-Poppins">{text} {children}</h2>
-    );
+const DashboardTitle = ({
+  text,
+  children,
+  windowTitle,
+}: {
+  text: string;
+  children: ReactNode;
+  windowTitle: string;
+}) => {
+  document.title = `${windowTitle ? windowTitle : "Z-commerce"} | Z-E-commerce`;
+  return (
+    <h2 className="text-xl font-Poppins">
+      {text} {children}
+    </h2>
+  );
 };
 
 export default DashboardTitle;
