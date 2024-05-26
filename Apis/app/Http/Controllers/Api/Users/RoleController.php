4fb47cf8e56bd339b0366ea2f3f88ab80e3a2bee
engr-akramulhoque <akramulhoque.engineer@gmail.com>
2024-05-26 18:26:43 +0600

    role controller modified and resource added

--- Apis/app/Http/Controllers/Api/Users/RoleController.php
+++ Apis/app/Http/Controllers/Api/Users/RoleController.php
@@ -46,51 +46,56 @@ class RoleController extends Controller
         $role = Role::create(array_merge($request->validated(), ['guard_name' => 'web']));
 
         $role->syncPermissions($request->permissions);
-        return Response::created($role, "Role created");
+        return Response::created(new RoleResource($role), "Role successfully created");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Role $role)
     {
-        $permissions = Permission::get(['id', 'name']);
-        $role = Role::with('permissions:id,name')->find($id, ['id', 'name']);
-        if ($role) {
-            $checkedPermissions = $role->permissions()->pluck('id')->toArray();
-            return Response::success([
-                'role' => $role,
-                'checkedPermissions' => $checkedPermissions,
-                'permissions' => $permissions
-            ]);
-        }
-        return Response::notFound("Role not found");
+        $data = [
+            'role' => [
+                'id' => $role->id,
+                'name' => $role->name,
+            ],
+            'total-permissions' => $role->permissions->count(),
+            'permissions' => $role->permissions->pluck('id', 'name'),
+        ];
+
+        return Response::success($data);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(RoleUpdateRequest $request, string $id)
+    public function update(RoleUpdateRequest $request, Role $role)
     {
-        $role = Role::find($id);
-        if ($role) {
-            $role->update($request->validated());
-            $role->syncPermissions($request->permissions);
-            return Response::updated($role, "Role Updated");
+        if ($role->id == 1) {
+            return Response::notFound("This role cannot be editable");
         }
-        return Response::notFound("Role not found");
+        try {
+            // update role
+            $role->update($request->validated());
+            // sync permissions
+            $role->syncPermissions($request->permissions);
+
+            return Response::updated($role, "Role successfully Updated");
+        } catch (\Exception $e) {
+            return Response::notFound("Role not found");
+        };
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Role $role)
     {
-        $role = Role::find($id);
-        if ($role) {
-            $role->delete();
-            return Response::success(null, "Role Deleted");
+        if ($role->id == 1) {
+            return Response::notFound("This role cannot be able to delete");
         }
-        return Response::notFound("Role not found");
+
+        $role->delete();
+        return Response::success(null, "Role successfully deleted");
     }
 }
