90e2deceb73233e2a08ab38eb0cdccb00517e91a
Ummepinki <salmaumme888@gmail.com>
2024-05-29 13:16:19 +0600

    brand updated and delete

--- client/src/Redux/Api/baseApi.ts
+++ client/src/Redux/Api/baseApi.ts
@@ -17,5 +17,5 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: baseQuery,
   endpoints: () => ({}),
-  tagTypes: ["categories", "users", "permissions", "roles"],
+  tagTypes: ["categories", "users", "permissions", "roles" , "brands"],
 });
