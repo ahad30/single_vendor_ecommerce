35f72dd61e3f8db7902e8dca16e6c0bfc500dfc4
unknown <naimurk781@gmail.com>
2024-06-02 12:35:45 +0600

    s

--- client/src/Component/Form/ZInput.tsx
+++ client/src/Component/Form/ZInput.tsx
@@ -2,26 +2,36 @@
 import { Form, Input } from "antd";
 import { useEffect } from "react";
 import { Controller, useFormContext } from "react-hook-form";
+import { RootState } from "../../Redux/store";
+import { useAppSelector } from "../../Redux/hook";
 
 const ZInput = ({
   name,
   type,
   label,
   value,
+  reset,
 }: {
   name: string;
   type: string;
   label: string;
   value?: string;
+  reset?: boolean;
 }) => {
-  const { control, setValue } = useFormContext();
-
+  const { control, setValue, resetField } = useFormContext();
+  const { isEditModalOpen } = useAppSelector((state: RootState) => state.modal);
   useEffect(() => {
     if (value) {
       setValue(name, value);
     }
-  }, [value , setValue]);
- 
+  }, [value, setValue]);
+  useEffect(() => {
+    if (reset === true) {
+      if (!isEditModalOpen) {
+        resetField(name);
+      }
+    }
+  }, [reset, isEditModalOpen]);
   return (
     <Controller
       name={name}
