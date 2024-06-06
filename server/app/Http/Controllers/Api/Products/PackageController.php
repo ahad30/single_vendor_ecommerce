42315f6658f28cd35a64fabb158351b46c712116
engr-akramulhoque <akramulhoque.engineer@gmail.com>
2024-06-06 16:37:02 +0600

    package without existing product done

--- server/app/Http/Controllers/Api/Products/PackageController.php
+++ server/app/Http/Controllers/Api/Products/PackageController.php
@@ -9,6 +9,8 @@ use App\Models\Package;
 use App\Trait\PaginationTrait;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class PackageController extends Controller
 {
@@ -30,9 +32,43 @@ class PackageController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
+    // generate slug for package
+    protected function generateSlug($name)
+    {
+        $slug = Str::slug($name);
+
+        if (Package::where('slug', $slug)->exists()) {
+            return Response::error('Package already exists', 409);
+        }
+
+        return $slug;
+    }
+    // check if package exists
+    public function checkPackageExists($package)
+    {
+    }
+
     public function store(StorePackageRequest $request)
     {
-        //
+        $slug = $this->generateSlug($request->name);
+        if ($slug instanceof \Illuminate\Http\JsonResponse) {
+            return $slug; // return the error response
+        }
+
+        // Start a database transaction
+        $data = DB::transaction(function () use ($request, $slug) {
+            // Create the package with the validated data and generate a slug
+            $package = Package::create(array_merge($request->validated(), ['slug' => $slug]));
+            // store items
+            $this->storeItems($request, $package);
+
+            // Return the created package
+            return $package;
+        });
+
+        // Return a success response with the created package data
+        return Response::success($data, "Package successfully created");
     }
 
     /**
@@ -64,5 +100,20 @@ class PackageController extends Controller
         // delete the customer
         // $customer->delete();
         // return Response::success(null, "Customer successfully deleted");
+    }
+
+    // Iterate over the items in the request to create related package items
+    protected function storeItems($request, $package)
+    {
+        foreach ($request['items'] as $item) {
+            $package->packageItems()->create([
+                "product_id" => $item['product_id'],
+                "product_name" => $item['product_name'],
+                "sku_id" => $item['sku_id'],
+                "code" => $item['code'],
+                "price" => $item['price'],
+                "quantity" => $item['quantity']
+            ]);
+        }
     }
 }
