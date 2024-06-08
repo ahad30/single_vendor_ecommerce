ac1ee1818b630fd57dd14fca1be172ccc273add0
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-06-08 17:59:29 +0600

    customer dashboard

--- client/src/Layout/Dashboard/CustomerDashboardLayout.tsx
+++ client/src/Layout/Dashboard/CustomerDashboardLayout.tsx
@@ -5,7 +5,7 @@ import CustomerDashboardSidebar from "./CustomerDashboardSidebar";
 const CustomerDashboardLayout = () => {
 
   return (
-    <div className="bg-[#f0f5fc] p-7 min-h-screen flex justify-between">
+    <div className="bg-[#f0f5fc] p-7 min-h-screen grid grid-cols-[1fr_3fr] gap-6">
       <div><CustomerDashboardSidebar /></div>
       <Outlet></Outlet>
     </div>
--- client/src/Layout/Dashboard/CustomerDashboardSidebar.tsx
+++ client/src/Layout/Dashboard/CustomerDashboardSidebar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { CustomerRoutes } from "../../Routes/Customer.Routes";
 import { sidebarGenerator } from "../../utils/sidebarGenerator";
 import { useState } from "react";
@@ -6,32 +6,34 @@ import { useState } from "react";
 
 const CustomerDashboardSidebar = () => {
     const sidebarData = sidebarGenerator(CustomerRoutes)
-    console.log(sidebarData)
+    // console.log(sidebarData)
     const [open, setOpen] = useState("");
-
+    const { pathname } = useLocation();
+    console.log(pathname);
     return (
-        <div className="bg-white w-[320px] px-7 rounded-md py-2">
+        <div className="bg-white w-[320px] rounded-md py-3">
             {/* thi div contains image and user name */}
             <div>
                 <div className="mb-5  flex flex-col justify-center items-center">
                     <img className="w-40 h-40" src="https://i.ibb.co/J3ChPyM/istockphoto-1451587807-612x612.jpg" alt="" />
-                    <h1>Sabiha Wasema</h1>
+                    <h1 className="text-[#265edd] font-medium text-[20px]">Sabiha Wasema</h1>
                 </div>
                 {/* sidebar data */}
                 <div>
-                    {
-                        sidebarData.map((item) => {
-                            return <Link to={item.key}>
-                                <div className="flex items-center gap-x-3 p-2 text-sm">
-                                    <span>{item?.icon}</span>
-                                    <span>{item?.label}</span>
+                    {sidebarData.map((item) => {
+
+                        return (
+                            <Link to={item.key} key={item.key} className="">
+                                <div className={`flex items-center hover:border-x-4 border-blue-500 duration-100 gap-x-3 px-7 py-1 text-sm hover:bg-gray-200 ${pathname === item.key ? " bg-gray-200 border-x-4 border-blue-500" : ""}`}>
+                                    <span className="py-2">{item?.icon}</span>
+                                    <span className="py-2">{item?.label}</span>
                                 </div>
                             </Link>
-                        })
-                    }
+                        );
+                    })}
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
