a96c02b90a3e777d8268cbe96dbcf230f568b063
sabiha-wasema <sabihawasemaa1244@gmail.com>
2024-06-10 09:54:07 +0600

    commit

--- client/src/Pages/Dashboard/User/Dashboard/CustomerDashboardStatics.tsx
+++ client/src/Pages/Dashboard/User/Dashboard/CustomerDashboardStatics.tsx
@@ -1,4 +1,5 @@
 import { CiBoxList } from "react-icons/ci";
+import { FaClipboardList } from "react-icons/fa";
 const CustomerDashboardStatics = () => {
   return (
     <div>
@@ -11,23 +12,23 @@ const CustomerDashboardStatics = () => {
           <p className="text-lg">All orders</p>
         </div>
         <div className="w-64 h-48 bg-white flex flex-col justify-center items-center rounded-lg"> <div className="w-12 h-10 bg-[#265eda] rounded-lg flex justify-center items-center text-white">
-          <CiBoxList className="w-8 h-8 " />
+          <FaClipboardList className="w-6 h-6 " />
         </div>
           <h1 className="text-2xl mt-2 font-bold">0 </h1>
-          <p className="text-lg">All orders</p></div>
+          <p className="text-lg">Pending orders</p></div>
         <div className="w-64 h-48 bg-white flex flex-col justify-center items-center rounded-lg">
           <div className="w-12 h-10 bg-[#265eda] rounded-lg flex justify-center items-center text-white">
             <CiBoxList className="w-8 h-8 " />
           </div>
           <h1 className="text-2xl mt-2 font-bold">0 </h1>
-          <p className="text-lg">All orders</p>
+          <p className="text-lg">Order Completed</p>
         </div>
         <div className="w-64 h-48 bg-white flex flex-col justify-center items-center rounded-lg">
           <div className="w-12 h-10 bg-[#265eda] rounded-lg flex justify-center items-center text-white">
             <CiBoxList className="w-8 h-8 " />
           </div>
           <h1 className="text-2xl mt-2 font-bold">0 </h1>
-          <p className="text-lg">All orders</p>
+          <p className="text-lg">Canceled Orders</p>
         </div>
       </div>
     </div>
