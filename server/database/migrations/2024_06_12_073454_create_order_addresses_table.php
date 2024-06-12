ed7552646c70f050f0a4cf912d177fb337c546e9
engr-akramulhoque <akramulhoque.engineer@gmail.com>
2024-06-12 17:40:19 +0600

    address controller added

--- server/database/migrations/2024_06_12_073454_create_order_addresses_table.php
+++ server/database/migrations/2024_06_12_073454_create_order_addresses_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->text('address')->nullable();
-            $table->boolen('is_default')->default(false);
+            $table->boolean('is_default')->default(false);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
