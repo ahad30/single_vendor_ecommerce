a5a0c2c0421e688ebabef7a3c96fc32454b5564e
unknown <naimurk781@gmail.com>
2024-06-22 12:00:34 +0600

    s

--- client/src/Pages/Dashboard/Admin/Category/Category.tsx
+++ client/src/Pages/Dashboard/Admin/Category/Category.tsx
@@ -41,14 +41,7 @@ const Category = () => {
   const { data, isLoading, isFetching } = useGetCategoriesQuery([
     { name: "page", value: pageNumber },
   ]);
-  const handleEditAndDelete = (data: TCategory, name: "delete" | "edit") => {
-    setSingleData(data);
-    if (name === "delete") {
-      dispatch(setIsDeleteModalOpen());
-    } else if (name === "edit") {
-      dispatch(setIsEditModalOpen());
-    }
-  };
+
 
   //
   const handleEdit = (data: TCategory, name: "edit") => {
